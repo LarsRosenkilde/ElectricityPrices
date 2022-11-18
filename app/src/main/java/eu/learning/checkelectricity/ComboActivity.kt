19275bcd2cd995ac89860a5f1a478ea23c66e904
LarsRosenkilde <lars.rosenkilde95@gmail.com>
2022-11-18 20:30:27 +0000

    Import cleanup

--- app/src/main/java/eu/learning/checkelectricity/ComboActivity.kt
+++ app/src/main/java/eu/learning/checkelectricity/ComboActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import eu.learning.checkelectricity.databinding.ActivityComboBinding
-import kotlinx.coroutines.flow.combine
 import org.jsoup.Jsoup
 import java.io.IOException
 
--- app/src/main/java/eu/learning/checkelectricity/MainActivity.kt
+++ app/src/main/java/eu/learning/checkelectricity/MainActivity.kt
@@ -1,17 +1,9 @@
 package eu.learning.checkelectricity
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import android.os.AsyncTask
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import eu.learning.checkelectricity.databinding.ActivityMainBinding
-import kotlinx.coroutines.flow.combine
-import org.jsoup.Jsoup
-import java.io.IOException
-
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
@@ -21,17 +13,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         title = "CheckElectricity"
+        val intentFlex = Intent(this, FlexActivity::class.java)
+        val intentCombo = Intent(this, ComboActivity::class.java)
+        val intentPool = Intent(this, PoolActivity::class.java)
         binding.btnFlexActivity.setOnClickListener {
-            val intent = Intent(this, FlexActivity::class.java)
-            startActivity(intent)
+            startActivity(intentFlex)
         }
         binding.btnComboActivity.setOnClickListener {
-            val intent = Intent(this, ComboActivity::class.java)
-            startActivity(intent)
+            startActivity(intentCombo)
         }
         binding.btnPoolActivity.setOnClickListener {
-            val intent = Intent(this, PoolActivity::class.java)
-            startActivity(intent)
+            startActivity(intentPool)
         }
     }
 }
