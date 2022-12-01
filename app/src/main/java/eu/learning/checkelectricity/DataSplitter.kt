b2f392a99eb0cd350ee65c7131fe8408a6ff03b1
LarsRosenkilde <lars.rosenkilde95@gmail.com>
2022-12-01 10:16:54 +0100

    DataSplitter Class

--- app/src/main/java/eu/learning/checkelectricity/DataSplitter.kt
+++ app/src/main/java/eu/learning/checkelectricity/DataSplitter.kt
@@ -10,23 +10,46 @@ class DataScraper (data: String) {
         val meanPrices: MutableList<Float> = mutableListOf()
         val dates: MutableList<String> = mutableListOf()
         for (i in 0 until splitData.count() step 3) {
-            meanPrices.add((splitData[i]
+            dates.add(splitData[i])
+            meanPrices.add((splitData[i + 1]
                 .replace(" øre/kWh", "")
                 .replace(',', '.')
-                .toFloat() + splitData[i + 1]
+                .toFloat() + splitData[i + 2]
                 .replace(" øre/kWh", "")
                 .replace(',', '.')
                 .toFloat()) / 2)
-            dates.add(splitData[i + 2])
         }
         return Pair(meanPrices, dates)
     }
 }
 
 fun main() {
-    val dummyData: String = "231,12 øre/kWh:134,22 øre/kWh:01/12/2022:241,12 øre/kWh:154,22 øre/kWh:02/12/2022:"
+    val dummyData: String = "02/12/2022:231,12 øre/kWh:134,22 øre/kWh:01/12/2022:241,12 øre/kWh:154,22 øre/kWh:"
     val (prices, dates) = DataScraper(dummyData).divide()
 
     println(prices)
     println(dates)
+
+    fun populateFields(dataString: String) {
+        val data: List<String> = dataString.dropLast(1).split(':')
+        val textFields: MutableMap<String, String> = mutableMapOf(
+            "date0" to "", "wOldPrice0" to "", "eOldPrice0" to "",
+            "date1" to "", "wOldPrice1" to "", "eOldPrice1" to "",
+            "date2" to "", "wOldPrice2" to "", "eOldPrice2" to "",
+            "date3" to "", "wOldPrice3" to "", "eOldPrice3" to "",
+            "date4" to "", "wOldPrice4" to "", "eOldPrice4" to "",
+        )
+        var counter = 0
+        textFields.forEach { elem ->
+            try {
+                textFields[elem.key] = data[counter++]
+            } catch (E: IndexOutOfBoundsException) {
+                textFields[elem.key] = "Unset Value"
+            }
+        }
+
+        println(textFields)
+    }
+
+    populateFields(dummyData)
 }
